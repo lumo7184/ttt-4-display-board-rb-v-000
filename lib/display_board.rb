62b8c47401059db3d8570955b498e49cabcdd37e
lucas montoni <lmontoni@outlook.com>
2018-08-07 02:06:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,17 @@
 # out the current state.
 the_board = [" "," "," "," "," "," "," "," "," "]
 def display_board(the_board)
+<<<<<<< HEAD
   puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]} "
   puts "-----------"
   puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]} "
   puts "-----------"
   puts " #{the_board[6]} | #{the_board[7]} | #{the_board[8]} "
+=======
+  puts " #{the_board[0]} |#{the_board[1]} |#{the_board[2]} "
+  puts "-----------"
+  puts " #{the_board[3]}|#{the_board[4]} | #{the_board[5]} "
+  puts "-----------"
+  puts " #{the_board[6]}|#{the_board[7]} | #{the_board[8]}  "
+>>>>>>> 654f9195e7d42e894baa722ed853fa679ab46248
 end
